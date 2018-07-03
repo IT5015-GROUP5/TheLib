f9ce59973970a5cc3fc14105d5f4054d3145b309
Mae Rhealyn Watin <maerhealynwatin@gmail.com>
2018-07-03 21:22:33 +0800

    Edited the register query code

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 
 	//////////////////SQL Query/////////////////
 	var_dump($_POST);
-	$searchquery = "SELECT username FROM account where username = '".$_POST['username']."'";
+	$searchquery = "SELECT username FROM user where username = '".$_POST['username']."'";
 	$result = mysqli_query($conn,$searchquery);
 	if (!$result) {
 		die("Error from query".$result->connect_error);
@@ -13,7 +13,7 @@
 	$row = mysqli_fetch_assoc($result);
 	var_dump($row, $result);
 	if ($row == NULL) {
-		$query = "INSERT INTO account (username, password, name) 
+		$query = "INSERT INTO user (username, password, name) 
 				  VALUES ('".$_POST['username']."', '".$_POST['pass']."', '".$_POST['name']."')";
 		var_dump($query);
 		$newresult = mysqli_query($conn,$query);
