ac27a2030f4a8dd467ecdd668d1ee2b1ddc4b6c4
Marian Isabel <yaneemozo@gmail.com>
2018-07-05 22:57:30 +0800

    Changed the books into a dynamic list
    Got books from the db itself. Please make the modal for the view books dynamic as well. I have placed an indication where the modal is and where the parts of the book info should be placed

--- home.php
+++ home.php
@@ -1,18 +1,26 @@
 <?php
 	session_start();
+	require("db_connect.php");
 
-	if(!isset($_SESSION['logged_in'])) {
-		header('Location:index.php');
-	}
+	$query = "SELECT book_name, author.author_name, year_pub, isbn FROM `books` JOIN author ON author.authorID = book_author";
+	$result = mysqli_query($conn, $query);
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Home</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="images/logo.png">
+	<link rel="stylesheet" type="text/css" href="css/navbar.css">
 	<link rel="stylesheet" type="text/css" href="css/home.css">
-	<?php
-		include('links.php');
-	?>
+	<link rel='stylesheet' href='datatable_files/datatables.min.css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+  	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
 </head>
 <body>
 	<div class="wrapper">
@@ -24,7 +32,7 @@
 				<li><a href="home.php" class="active">Home</a></li>
 				<li><a href="add_books.php">Add Books</a></li>
 				<li><a href="logs.php">Logs</a></li>
-				<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span></a></li>
+				<li><a href="index.php"><span class="glyphicon glyphicon-log-out"></span></a></li>
 			</ul>
 		</nav>
 		<section class="sec1"></section>
@@ -45,90 +53,23 @@
 								</tr>
 							</thead>
 							<tbody>
-								<tr>
-									<td>Harry Potter and The Sorcerer's Stone</td>
-									<td><center>J.K. Rowling</center></td>
-									<td><center>1997</center></td>
-									<td><center>0-7475-3269-9</center></td>
-									<td>
-										<center>
-											<button class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>
-											<button class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></button>
-										</center>
-									</td>
-								</tr>
-								<tr>
-									<td>Harry Potter and The Chamber of Secrets</td>
-									<td><center>J.K. Rowling</center></td>
-									<td><center>1998</center></td>
-									<td><center>9-7804-3906-486-6</center></td>
-									<td>
-										<center>
-											<button class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>
-											<button class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></button>
-										</center>
-									</td>
-								</tr>
-								<tr>
-									<td>Harry Potter and The Prisoner of Azkaban</td>
-									<td><center>J.K. Rowling</center></td>
-									<td><center>1999</center></td>
-									<td><center>9-7814-0885-567-6</center></td>
-									<td>
-										<center>
-											<button class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>
-											<button class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></button>
-										</center>
-									</td>
-								</tr>
-								<tr>
-									<td>Harry Potter and The Goblet of Fire</td>
-									<td><center>J.K. Rowling</center></td>
-									<td><center>2000</center></td>
-									<td><center>9-7807-4755-099-0</center></td>
-									<td>
-										<center>
-											<button class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>
-											<button class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></button>
-										</center>
-									</td>
-								</tr>
-								<tr>
-									<td>Harry Potter and The Order of Phoenix</td>
-									<td><center>J.K. Rowling</center></td>
-									<td><center>2003</center></td>
-									<td><center>9-7817-8110-053-0</center></td>
-									<td>
-										<center>
-											<button class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>
-											<button class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></button>
-										</center>
-									</td>
-								</tr>
-								<tr>
-									<td>Harry Potter and The Half Blood Prince</td>
-									<td><center>J.K. Rowling</center></td>
-									<td><center>2005</center></td>
-									<td><center>9-7814-0885-594-2</center></td>
-									<td>
-										<center>
-											<button class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>
-											<button class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></button>
-										</center>
-									</td>
-								</tr>
-								<tr>
-									<td>Harry Potter and The Deathly Hallows</td>
-									<td><center>J.K. Rowling</center></td>
-									<td><center>2007</center></td>
-									<td><center>9-7805-4501-022-1</center></td>
-									<td>
-										<center>
-											<button class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>
-											<button class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></button>
-										</center>
-									</td>
-								</tr> 
+								<?php
+									while($row = mysqli_fetch_array($result))
+									{
+										echo "<tr>";
+											echo "<td><a type='button' data-toggle='modal' data-target='#bookModal'>" .$row['book_name']. "</a></td>";
+											echo "<td><center>" .$row['author_name']. "</center></td>";
+											echo "<td><center>" .$row['year_pub']. "</center></td>";
+											echo "<td><center>" .$row['isbn']. "</center></td>";
+											echo "<td>
+													<center>
+														<button class='btn btn-warning btn-xs'><span class='glyphicon glyphicon-pencil'></span></button>
+														<button class='btn btn-danger btn-xs'><span class='glyphicon glyphicon-trash'></span></button>
+													</center>
+												</td>";
+										echo "</tr>";
+									}
+								?>
 							</tbody>
 						</table>
 					</div>
@@ -137,6 +78,33 @@
 			</div>
 		</section>
 	</div>
+
+	<!-- MODAL FOR THE BOOK INFORMATION -->
+	<div class="modal fade" id="bookModal" role="dialog">
+	    <div class="modal-dialog">
+		      <!-- Modal content-->
+		      <div class="modal-content">
+		        <div class="modal-header">
+		          <button type="button" class="close" data-dismiss="modal">&times;</button>
+		          <p class="modal-title bookInfo-modal-page-label">Book Information</p>
+		        </div>
+		        <!-- Place book information inside this div using Php -->
+		        <div class="modal-body">
+			    	<div class="book-name-modal">
+			    		<p><strong>Book Name</strong></p>
+			    		<?php
+
+			    		?>
+			    	</div>
+		        </div>
+		        <div class="modal-footer">
+		        	<button type="submit" class="btn btn-success register-modal-btn">Register</button>
+		          <button type="button" class="btn btn-default register-modal-close" data-dismiss="modal">Close</button>
+		        </div>
+		      </div>
+	    </div>
+	</div>
+
 </body>
 </html>
 <script type="text/javascript">
@@ -149,6 +117,8 @@
 		}
 	});
 </script>
+<script src='datatable_files/jquery.min.js'></script>
+<script src='datatable_files/datatables.min.js'></script>
 <script>
 	$(document).ready(function(){
 		$("#bookTable").DataTable({
