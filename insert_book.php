dd0e36d1655728d663687ba1b87f88f952644927
Marian Isabel <yaneemozo@gmail.com>
2018-07-06 12:44:56 +0800

    Changes in the insertion
    As there has been a change in db, I also incorporated the changes in for the insertion. Please use the latest db

--- insert_book.php
+++ insert_book.php
@@ -13,25 +13,32 @@ if(isset($_POST['bookTitle']) && isset($_POST['fName']) && isset($_POST['lName']
 	$page = $_POST['pageCount'];
 	$pub = $_POST['pubYear'];
 
+	//ACTION
+	$actionQuery = "SELECT * FROM actions WHERE action_name='Added Book'";
+	$actionResult = mysqli_query($conn, $actionQuery);
+	$actionRow = mysqli_fetch_assoc($actionResult);
+	$actionIDGot = $actionRow["actionID"];
+
 	//FOR LOG DETAILS
 	$user = $_POST['user'];
-	$userQuery = "SELECT userId FROM User WHERE username = '".$user."'";
+	$userQuery = "SELECT userId FROM user WHERE username = '".$user."'";
 	$userResult = mysqli_query($conn,$userQuery);
 	$userRow = mysqli_fetch_assoc($userResult);
-	$action = "Added Book";
 	$description = "Added ".$title;
+	$dateTime = date("Y-m-d H:i:s");
+
 
 	if($userRow != NULL){
 		$userId = $userRow['userId'];
 	}
 
 	//FOR BOOK DETAILS CHECKS IF AUTHOR EXISTS IN DB
-	$authorQuery1 = "SELECT authorId FROM Author WHERE firstName = '".$fname."' AND lastName = '".$lname."'";
+	$authorQuery1 = "SELECT authorID FROM author WHERE firstName = '".$fname."' AND lastName = '".$lname."'";
 	$authorResult = mysqli_query($conn,$authorQuery1);
 
 	//IF AUTHOR DOES NOT EXIST, AUTHOR IS ADDED TO THE DB
 	if(mysqli_num_rows($authorResult)==0){
-		$authorQuery2 = "INSERT INTO Author (firstName,lastName) VALUES ('".$fname."','".$lname."')";
+		$authorQuery2 = "INSERT INTO author (firstName,lastName) VALUES ('".$fname."','".$lname."')";
 		//GETS THE ID OF NEWLY ADDED AUTHOR
 		if(mysqli_query($conn,$authorQuery2)){
 			$authorId = mysqli_insert_id($conn);
@@ -41,22 +48,36 @@ if(isset($_POST['bookTitle']) && isset($_POST['fName']) && isset($_POST['lName']
 	}else{
 		$row = mysqli_fetch_assoc($authorResult);
 		if($row != NULL){
-			$authorId = $row['authorId'];
+			$authorId = $row['authorID'];
 		}
 	}
 
 	//INSERT BOOK QUERY
-	$query2 = "INSERT INTO Book (title,authorId,pubDate,ISBN,description,pageCount) VALUES ('".$title."',".$authorId.",".$pub.",'".$isbn."','".$desc."',".$page.")";
+	$query2 = "INSERT INTO books (book_name, year_pub, isbn, description, page_number, book_author) 
+						VALUES ('".$title."',".$pub.",".$isbn.",'".$desc."','".$page."',".$authorId.")";
+
+	//GET ID OF BOOK RECENTLY INSERTED
+	$bookIDQuery = "SELECT * FROM books WHERE book_name='".$title."'";
+	$bookIDResult = mysqli_query($conn, $bookIDQuery);
+	$bookIDRow = mysqli_fetch_assoc($bookIDResult);
+	$boodIDInserted = $bookIDRow["bookID"];
+
 	//AFTER INSERTING BOOK QUERY
 	if(mysqli_query($conn,$query2)){
 		//INSERT LOG QUERY
-		$logQuery = "INSERT INTO Logs (action,description,userId) VALUES ('".$action."','".$description."',".$userId.")";
-		mysqli_query($conn,$logQuery);
-
-		echo "<script> 
+		$logQuery = "INSERT INTO logs (accountID, bookID, actionID, action, time_of_action) 
+						VALUES ('".$userId."','".$boodIDInserted."','".$actionIDGot."','".$description."','".$dateTime."')";
+		if(mysqli_query($conn,$logQuery)){
+			echo "<script> 
 				alert('Successfully Added to the Library');
 				window.location.href='add_books.php';
 			</script>";
+		}else{
+			echo "<script> 
+				alert('Error');
+				window.location.href='add_books.php';
+			</script>";
+		}
 	}else{
 		echo "<script> 
 				alert('Error');
