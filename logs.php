48a5833c1136bff76a2fcfa6f036946abce7118a
Mae Rhealyn Watin <maerhealynwatin@gmail.com>
2018-07-07 23:03:51 +0800

    Cleaned the code

--- logs.php
+++ logs.php
@@ -19,17 +19,10 @@
 <html>
 <head>
 	<title>Logs</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="images/logo.png">
-	<link rel="stylesheet" type="text/css" href="css/navbar.css">
 	<link rel="stylesheet" type="text/css" href="css/logs.css">
-	<link rel='stylesheet' href='datatable_files/datatables.min.css'>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-  	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  	<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+	<?php
+		include('links.php');
+	?>
 </head>
 <body>
 	<div class="wrapper">
@@ -91,10 +84,7 @@
 			$('nav').removeClass('black');
 		}
 	});
-</script>
-<script src='datatable_files/jquery.min.js'></script>
-<script src='datatable_files/datatables.min.js'></script>
-<script>
+	
 	$(document).ready(function(){
 		$("#logsList").DataTable({
 			"pagingType": "simple"
