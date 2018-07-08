f3bef801cb057aa4dc7fa65a990d1e709ca9e128
Mae Rhealyn Watin <maerhealynwatin@gmail.com>
2018-07-08 10:23:30 +0800

    Updated sql

--- distributedsql.sql
+++ distributedsql.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 06, 2018 at 06:47 AM
+-- Generation Time: Jul 08, 2018 at 04:23 AM
 -- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- PHP Version: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,82 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `distributedsql`
+-- Database: `library`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `actions`
---
-
-CREATE TABLE `actions` (
-  `actionID` int(11) NOT NULL,
-  `action_name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `actions`
---
-
-INSERT INTO `actions` (`actionID`, `action_name`) VALUES
-(1, 'Added Book'),
-(2, 'Edited Book'),
-(3, 'Deleted Book');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `author`
---
-
-CREATE TABLE `author` (
-  `authorID` int(11) NOT NULL,
-  `firstName` varchar(50) NOT NULL,
-  `lastName` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `author`
---
-
-INSERT INTO `author` (`authorID`, `firstName`, `lastName`) VALUES
-(1, 'J.K.', 'Rowling'),
-(3, 'Rick', 'Riordan');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `books`
---
-
-CREATE TABLE `books` (
-  `bookID` int(11) NOT NULL,
-  `book_name` varchar(150) NOT NULL,
-  `year_pub` int(5) NOT NULL,
-  `isbn` varchar(60) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `page_number` int(11) NOT NULL,
-  `book_author` int(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `books`
---
-
-INSERT INTO `books` (`bookID`, `book_name`, `year_pub`, `isbn`, `description`, `page_number`, `book_author`) VALUES
-(1, 'Harry Potter and The Sorcerer''s Stone', 1997, '0-7475-3269-9', 'Harry Potter and The Sorcerer''s Stone', 333, 1),
-(2, 'Harry Potter and The Chamber of Secrets', 1998, '9-7804-3906-486-6', 'Harry Potter and The Chamber of Secrets', 500, 1),
-(3, 'Harry Potter and The Prisoner of Azkaban', 1999, '9-7814-0885-567-6', 'Harry Potter and The Prisoner of Azkaban', 654, 1),
-(4, 'Harry Potter and The Goblet of Fire', 2000, '9-7807-4755-099-0', 'Harry Potter and The Goblet of Fire', 655, 1),
-(5, 'Harry Potter and The Order of Phoenix', 2003, '9-7817-8110-053-0', 'Harry Potter and The Order of Phoenix', 600, 1),
-(6, 'Harry Potter and The Half Blood Prince', 2005, '9-7814-0885-594-2', 'Harry Potter and The Half Blood Prince', 780, 1),
-(7, 'Harry Potter and The Deathly Hallows', 2007, '9-7805-4501-022-1', 'Harry Potter and The Deathly Hallows', 954, 1),
-(8, 'Fantastic Beasts and Where to Find Them', 2001, '-11093', 'Fantastic Beasts and Where to Find Them is a 2001 book written by British author J. K. Rowling about the magical creatures in the Harry Potter universe.', 158, 1),
-(9, 'The Son of Neptune', 2011, '-6277', 'The Son of Neptune is a 2011 fantasy-adventure novel written by American author Rick Riordan, based on Greek and Roman mythology. It is the second book in The Heroes of Olympus series, preceded by The Lost Hero and followed by The Mark of Athena.', 513, 3),
-(11, 'The Lost Hero', 2010, '-12828', 'The Lost Hero is an American fantasy-adventure novel written by Rick Riordan, based on Greek and Roman mythology.', 557, 3),
-(12, 'The Mark of Athena', 2012, '-1803', 'The Mark of Athena is an American fantasy-adventure novel written by Rick Riordan, based on Greek and Roman mythology.', 586, 3),
-(13, 'The Mark of Athena', 2012, '-1803', 'The Mark of Athena is an American fantasy-adventure novel written by Rick Riordan, based on Greek and Roman mythology.', 586, 3);
 
 -- --------------------------------------------------------
 
@@ -114,53 +40,22 @@ CREATE TABLE `logs` (
 --
 
 INSERT INTO `logs` (`logID`, `accountID`, `bookID`, `actionID`, `action`, `time_of_action`) VALUES
-(2, 3, 4, 1, 'Added the fourth book of HP', '2018-07-06 11:36:34'),
-(6, 3, 12, 1, 'Added The Mark of Athena', '2018-07-06 06:44:52');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `userId` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`userId`, `name`, `username`, `password`) VALUES
-(1, 'Janine Cabahug', 'Janine', 'janine123'),
-(2, 'Mae Rhealyn Watin', 'MimiWatin', 'mimi12345'),
-(3, 'Marian Isabel', 'marianmozo', 'qwert123');
+(1, 3, 4, 1, 'Added the fourth book of HP', '2018-07-06 11:36:34'),
+(2, 3, 12, 1, 'Added The Mark of Athena', '2018-07-06 06:44:52'),
+(3, 1, 14, 1, 'Added The Lighting Thief', '2018-07-07 20:08:47'),
+(5, 1, 4, 2, 'Edited Harry Potter and The Goblet of Fire', '2018-07-07 21:17:01'),
+(8, 2, 20, 3, 'Deleted Harry Potter and The Prisoner of Azkaban', '2018-07-08 02:48:08'),
+(9, 2, 14, 2, 'Edited The Lighting Thief', '2018-07-08 02:51:01'),
+(10, 2, 21, 1, 'Added Harry Potter and The Prisoner of Azkaban', '2018-07-08 03:00:01'),
+(48, 2, 24, 1, 'Added The Sea of Monsters', '2018-07-08 03:25:03'),
+(49, 2, 24, 3, 'Deleted The Sea of Monsters', '2018-07-08 03:25:09'),
+(50, 2, 25, 2, 'Edited The Sea of Monsters', '2018-07-08 03:25:42'),
+(60, 2, 0, 1, 'Added Twilight', '2018-07-08 04:17:25'),
+(61, 2, 29, 2, 'Edited Twilight', '2018-07-08 04:17:33');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `actions`
---
-ALTER TABLE `actions`
-  ADD PRIMARY KEY (`actionID`);
-
---
--- Indexes for table `author`
---
-ALTER TABLE `author`
-  ADD PRIMARY KEY (`authorID`);
-
---
--- Indexes for table `books`
---
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`bookID`),
-  ADD KEY `book_author` (`book_author`);
 
 --
 -- Indexes for table `logs`
@@ -172,58 +67,14 @@ ALTER TABLE `logs`
   ADD KEY `actionID` (`actionID`);
 
 --
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userId`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `actions`
---
-ALTER TABLE `actions`
-  MODIFY `actionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `author`
---
-ALTER TABLE `author`
-  MODIFY `authorID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `books`
---
-ALTER TABLE `books`
-  MODIFY `bookID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `logID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `books`
---
-ALTER TABLE `books`
-  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`book_author`) REFERENCES `author` (`authorID`);
-
---
--- Constraints for table `logs`
---
-ALTER TABLE `logs`
-  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`accountID`) REFERENCES `user` (`userId`),
-  ADD CONSTRAINT `logs_ibfk_2` FOREIGN KEY (`bookID`) REFERENCES `books` (`bookID`),
-  ADD CONSTRAINT `logs_ibfk_3` FOREIGN KEY (`actionID`) REFERENCES `actions` (`actionID`);
-
+  MODIFY `logID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
